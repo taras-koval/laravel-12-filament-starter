7098c84dc22fd3c7d5430d708384976cf2a5986e
taras-koval <taras.k@gtmarketing.ro>
2025-03-24 01:04:06 +0100

    Profile dropdown

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -16,7 +16,7 @@
 </head>
 <body class="flex flex-col min-h-screen">
 <header class="bg-zinc-800 text-white">
-    <nav class="container flex justify-between h-16">
+    <nav class="container relative flex justify-between h-16">
         <div class="flex items-center gap-x-8">
             <a href="{{ route('index') }}" class="text-2xl tracking-tighter font-black leading-tight">
                 {{ config('app.name') }}
@@ -33,21 +33,46 @@
                     $user = auth()->user();
                 @endphp
 
-                <a href="{{ route('profile.dashboard') }}" class="inline-flex items-center hover:bg-zinc-700 rounded-lg gap-2.5 px-2.5 py-1.5">
-                    @if($user->avatar)
-                        <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="size-8 rounded-full">
-                    @else
-                        <svg class="size-8 text-zinc-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
-                            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd"/>
-                        </svg>
-                    @endif
-                    {{ $user->name }}
-                </a>
+                {{-- Profile Dropdown --}}
+                <div x-data="{openDropdown: false}">
+                    {{-- Dropdown Button --}}
+                    <button @click="openDropdown = !openDropdown" class="inline-flex items-center hover:bg-zinc-700 rounded-lg px-2.5 py-1.5">
+                        @if($user->avatar)
+                            <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="size-8 rounded-full mr-2.5">
+                        @else
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
+                                 class="size-8 text-zinc-300 mr-2.5">
+                                <path d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653ZM6.145 17.812A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"/>
+                            </svg>
+                        @endif
 
-                <form action="{{ route('logout') }}" method="post">
-                    @csrf
-                    <button type="submit" class="cursor-pointer hover:underline">{{ __('Logout') }}</button>
-                </form>
+                        {{ $user->name }}
+
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 18" fill="none" stroke-width="3.5" stroke="currentColor" aria-hidden="true" class="size-3.5 ml-1.5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </button>
+
+                    {{-- Dropdown Panel --}}
+                    <div class="absolute right-4 top-14 font-normal bg-white w-48 rounded-lg shadow p-2 [&>a]:text-zinc-800"
+                         x-show="openDropdown" @click.outside="openDropdown = false" x-transition.duration.75 x-cloak>
+
+                        <a href="{{ route('profile.dashboard') }}" class="button-link-component">
+                            {{ __('My Profile') }}
+                        </a>
+                        <a href="{{ route('profile.account.edit') }}" class="button-link-component">
+                            {{ __('Account Settings') }}
+                        </a>
+
+                        <div class="border-t border-gray-200 my-1"></div>
+
+                        <form action="{{ route('logout') }}" method="post">
+                            @csrf
+                            <button type="submit" class="button-link-component text-zinc-800">{{ __('Logout') }}</button>
+                        </form>
+                    </div>
+
+                </div>
             @endauth
         </div>
     </nav>
