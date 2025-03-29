39b483cd26f79fe9cedda3783bb1cfcfe62cb905
taras-koval <tkoval37@gmail.com>
2025-03-29 19:25:46 +0100

    Install Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
+use App\Enums\UserRoleEnum;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail, FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory;
@@ -53,5 +56,10 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasRole(UserRoleEnum::ADMINISTRATOR);
     }
 }
